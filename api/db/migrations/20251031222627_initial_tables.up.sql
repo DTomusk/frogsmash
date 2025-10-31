6bca25ffa3ba8b6d7c5c6d3f0dc9d5ff91e5a128
Dave <david.tomusk@gmail.com>
2025-10-31 22:31:12 +0000

    Add initial migrations for items and events tables

---
+++ api/db/migrations/20251031222627_initial_tables.up.sql
@@ -0,0 +1,18 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS events (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    winner_id UUID NOT NULL,
+    loser_id UUID NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    processed_at TIMESTAMPTZ,
+    FOREIGN KEY (winner_id) REFERENCES items(id),
+    FOREIGN KEY (loser_id) REFERENCES items(id)
+);
+
+CREATE TABLE IF NOT EXISTS items (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    image_url TEXT NOT NULL,
+    score DECIMAL(8, 4) NOT NULL
+);
