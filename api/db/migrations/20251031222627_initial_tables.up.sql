663419d6d1f5732825744268ab4b6b5044bc6b6a
Dave <david.tomusk@gmail.com>
2025-11-01 09:15:24 +0000

    Fix migration order

--- api/db/migrations/20251031222627_initial_tables.up.sql
+++ api/db/migrations/20251031222627_initial_tables.up.sql
@@ -1,5 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TABLE IF NOT EXISTS items (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    image_url TEXT NOT NULL,
+    score DECIMAL(8, 4) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS events (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     winner_id UUID NOT NULL,
@@ -8,11 +15,4 @@ CREATE TABLE IF NOT EXISTS events (
     processed_at TIMESTAMPTZ,
     FOREIGN KEY (winner_id) REFERENCES items(id),
     FOREIGN KEY (loser_id) REFERENCES items(id)
-);
-
-CREATE TABLE IF NOT EXISTS items (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name TEXT NOT NULL,
-    image_url TEXT NOT NULL,
-    score DECIMAL(8, 4) NOT NULL
 );
