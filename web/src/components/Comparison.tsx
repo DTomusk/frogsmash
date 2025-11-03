4bb038aa3a2d9eae18bc4e31a170513f5c42d39c
Dave <david.tomusk@gmail.com>
2025-11-03 23:18:10 +0000

    Set up router and template

---
+++ web/src/components/Comparison.tsx
@@ -0,0 +1,24 @@
+import { useItems } from "../hooks/useItems"
+
+function Comparison() {
+  const { isPending, error, data } = useItems();
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error loading items.</div>;
+  }
+
+  const { left_item, right_item } = data;
+
+  return (
+    <div className="flex gap-4">
+      <img src={left_item.image_url} alt={left_item.name} />
+      <img src={right_item.image_url} alt={right_item.name} />
+    </div>
+  );
+}
+
+export default Comparison;
