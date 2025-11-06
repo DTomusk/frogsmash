b459fc5933a447c8fc81ca73f756648f92254375
Dave <david.tomusk@gmail.com>
2025-11-06 23:34:26 +0000

    Add loading page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Comparison from "./components/Comparison";
 import Template from "./components/Template";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
   return (<Routes>
     <Route element={<Template />}>
         <Route path='/' element={<Comparison />} />
+        <Route path='/loading' element={<LoadingSpinner />} />
     </Route>
   </Routes>
   );
