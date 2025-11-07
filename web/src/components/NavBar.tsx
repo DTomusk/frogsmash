e5bbb950e8655a1168122dbdfd0e8fd7a2f0720c
Dave <david.tomusk@gmail.com>
2025-11-07 20:34:37 +0000

    Make nav title home link

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,11 +1,14 @@
 import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function NavBar() {
     return (<AppBar position="fixed" color="primary">
         <Toolbar>
+            <Link to='/' style={{ textDecoration: 'none', color: 'inherit' }}>
             <Typography variant='h4'>
                 🐸 FrogSmash
             </Typography>
+            </Link>
         </Toolbar>
     </AppBar>);
 }
