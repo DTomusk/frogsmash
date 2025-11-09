fd91d45146391f06677e9450b60336802b8ff1a1
Dave <david.tomusk@gmail.com>
2025-11-09 12:44:57 +0000

    Vite config changes

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // ensures Vite is reachable from Docker
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://api:8080',  // <--- Docker Compose service name
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
