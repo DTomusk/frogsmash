bb8b040b6dd650d663da1d60489aea24a78e1470
Dave <david.tomusk@gmail.com>
2025-11-10 19:53:50 +0000

    Leaderboard entry styling

--- web/src/components/LeaderboardEntry.tsx
+++ web/src/components/LeaderboardEntry.tsx
@@ -1,17 +1,21 @@
-import { Avatar, ListItem, ListItemAvatar, ListItemText, Paper } from "@mui/material";
+import { Avatar, Box, ListItem, ListItemAvatar, ListItemText, Paper } from "@mui/material";
 import type { LeaderboardItem } from "../models/items";
 
 function LeaderboardEntry({ item }: { item: LeaderboardItem }) {
     return (
-        <Paper sx={{ marginBottom: 2, padding: 1 }}>
-        <ListItem>
+        <Paper sx={{ marginBottom: 2, padding: 1, width: '100%' }}>
+        <ListItem sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Box sx={{ display: 'flex', alignItems: 'center', width: '40%' }}>
+            <ListItemText primary={item.rank}/>
             <ListItemAvatar>
                 <Avatar src={item.image_url} alt={item.name} />
             </ListItemAvatar>
+            
             <ListItemText
-                primary={`Rank: ${item.rank} - ${item.name}`}
-                secondary={`Score: ${item.score}`}
+                primary={item.name}
             />
+            </Box>
+            <ListItemText primary={`Score: ${item.score}`} sx={{ flex: 1, textAlign: 'right' }} />
         </ListItem>
         </Paper>
     );
--- web/src/components/pages/LeaderboardPage.tsx
+++ web/src/components/pages/LeaderboardPage.tsx
@@ -1,4 +1,4 @@
-import { Pagination, Typography } from "@mui/material";
+import { List, Pagination, Typography } from "@mui/material";
 import { useLeaderboard } from "../../hooks/useLeaderboard";
 import LoadingSpinner from "../LoadingSpinner";
 import LeaderboardEntry from "../LeaderboardEntry";
@@ -6,19 +6,21 @@ import { useState } from "react";
 
 function LeaderboardPage() {
     const [page, setPage] = useState(1);
-    const { data, isLoading } = useLeaderboard(page, 10);
+    const { data: leaderboardData, isLoading } = useLeaderboard(page, 10);
 
   return <>
-    <Typography variant="h3" sx={{mt: 4}}>Leaderboard Page</Typography>
-    {isLoading && <LoadingSpinner />}
-    {!isLoading && (<>
-    <ul>
-      {data?.data?.map((item) => (
-        <LeaderboardEntry key={item.id} item={item} />
-      ))}
-    </ul>
-    <Pagination color="primary" count={data?.total_pages} page={page} onChange={(_, value) => setPage(value)} /></>)}
-  </>;
+        <Typography variant="h3" sx={{mt: 4, mb: 2}}>Leaderboard</Typography>
+        <Typography variant="subtitle1" sx={{mb: 2}}>🐸See who the strongest contenders are!🐸</Typography>
+        {isLoading && <LoadingSpinner />}
+        {!isLoading && (<>
+            <List sx={{ width: { xs: '100%', sm: '80%', md: '60%' }, mt: 4, mb: 4 }}>
+            {leaderboardData?.data?.map((item) => (
+                <LeaderboardEntry key={item.id} item={item} />
+            ))}
+            </List>
+            {leaderboardData!.total_pages > 1 && <Pagination color="primary" count={leaderboardData?.total_pages} page={page} onChange={(_, value) => setPage(value)} />}
+        </>)}
+    </>;
 }
 
 export default LeaderboardPage;
