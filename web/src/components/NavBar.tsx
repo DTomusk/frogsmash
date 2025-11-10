bd9f25c9a4fa12bb57378d5737ce5c6f4ee260ae
Dave <david.tomusk@gmail.com>
2025-11-10 20:00:36 +0000

    Add leaderboard link to nav

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -3,11 +3,16 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
     return (<AppBar position="fixed" color="primary">
-        <Toolbar>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
             <Link to='/' style={{ textDecoration: 'none', color: 'inherit' }}>
-            <Typography variant='h4'>
-                🐸 FrogSmash
-            </Typography>
+                <Typography variant='h4'>
+                    🐸 FrogSmash
+                </Typography>
+            </Link>
+            <Link to='/leaderboard' style={{ color: 'inherit' }}>
+                <Typography variant='h6'>
+                    Leaderboard
+                </Typography>
             </Link>
         </Toolbar>
     </AppBar>);
