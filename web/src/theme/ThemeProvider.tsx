ea032371ec844ae8d796100c0aea1ab628acfd7f
Dave <david.tomusk@gmail.com>
2025-11-11 11:50:04 +0000

    Read theme mode from local storage

--- web/src/theme/ThemeProvider.tsx
+++ web/src/theme/ThemeProvider.tsx
@@ -1,5 +1,5 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createContext, useContext, useMemo, useState, type FC, type ReactNode } from "react";
+import { createContext, useContext, useEffect, useMemo, useState, type FC, type ReactNode } from "react";
 import { darkTheme, lightTheme } from "./theme";
 
 type ColorMode = 'light' | 'dark';
@@ -17,13 +17,23 @@ const ThemeContext = createContext<ThemeContextType>({
 export const useThemeMode = () => useContext(ThemeContext);
 
 export const AppThemeProvider: FC<{ children: ReactNode }> = ({ children }) => {
-    const [mode, setMode] = useState<ColorMode>('light');
+    const [mode, setMode] = useState<ColorMode>(() => {
+        const savedMode = localStorage.getItem('themeMode') as ColorMode | null;
+        return savedMode ? savedMode : 'light';
+    });
 
     const toggleColorMode = () => {
         setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
     };
 
     const theme = useMemo(() => (mode === "light" ? lightTheme : darkTheme), [mode]);
+
+    useEffect(() => {
+        const storedMode = localStorage.getItem('themeMode');
+        if (storedMode !== mode) {
+            localStorage.setItem('themeMode', mode);
+        }
+    }, [mode]);
 
     return (
         <ThemeContext.Provider value={{ mode, toggleColorMode }}>
