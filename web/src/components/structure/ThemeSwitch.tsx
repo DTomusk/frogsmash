d207f05684cbcfb216904c47b02ef43a29dffb4d
Dave <david.tomusk@gmail.com>
2025-11-11 11:43:11 +0000

    Add icons to theme switch and remove text

--- web/src/components/structure/ThemeSwitch.tsx
+++ web/src/components/structure/ThemeSwitch.tsx
@@ -1,5 +1,36 @@
-import { Box, Typography, Switch, useTheme } from "@mui/material";
+import { Box, Switch, useTheme, styled } from "@mui/material";
 import { useThemeMode } from "../../theme/ThemeProvider";
+
+const StyledSwitch = styled(Switch)(() => ({
+    width: 56,
+    height: 32,
+    padding: 8,
+    "& .MuiSwitch-thumb": {
+        width: 24,
+        height: 24,
+        position: "relative",
+        "&::before": {
+        content: "'🌞'",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        fontSize: "20px",
+        },
+    },
+    "& .MuiSwitch-switchBase": {
+        padding: 3,
+        "&.Mui-checked": {
+            transform: "translateX(22px)",
+        },
+    },
+    "& .Mui-checked .MuiSwitch-thumb::before": {
+        content: "'🌜'", 
+    },
+    "& .MuiSwitch-track": {
+        borderRadius: 38 / 2,
+    },
+}));
 
 function ThemeSwitch() {
     const theme = useTheme();
@@ -7,11 +38,7 @@ function ThemeSwitch() {
 
     return (
         <Box sx={{ display: 'flex', alignItems: 'center', ml: 2 }}>
-            { theme.palette.mode === 'light' ? 
-                <Typography variant='h6'>Light mode</Typography> : 
-                <Typography variant='h6'>Dark mode</Typography> 
-            }
-            <Switch checked={theme.palette.mode === 'dark'} onChange={toggleColorMode} />
+            <StyledSwitch checked={theme.palette.mode === 'dark'} onChange={toggleColorMode} />
         </Box>
     );
 }
