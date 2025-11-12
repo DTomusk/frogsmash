c46897da947c3b2dafdab07f6a6e3dfc388e2ed2
Dave <david.tomusk@gmail.com>
2025-11-12 13:10:39 +0000

    Add gap to nav links

--- web/src/components/structure/NavLinks.tsx
+++ web/src/components/structure/NavLinks.tsx
@@ -1,17 +1,22 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import ThemeSwitch from "./ThemeSwitch";
 
 function NavLinks() {
     return (
-        <>
+        <Box sx={{ display: 'flex', gap: 4, alignItems: 'center' }}>
             <Link to='/leaderboard' style={{ color: 'inherit' }}>
                 <Typography variant='h6'>
                     Leaderboard
                 </Typography>
             </Link>
+            <Link to='/upload' style={{ color: 'inherit' }}>
+                <Typography variant='h6'>
+                    Upload
+                </Typography>
+            </Link>
             <ThemeSwitch />
-        </>
+        </Box>
     );
 }
 
