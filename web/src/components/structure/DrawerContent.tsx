7193092f18604a209c9e4f8e01abad0f241f3ad9
Dave <david.tomusk@gmail.com>
2025-11-14 11:47:43 +0000

    Conditional nav links based on token presence

--- web/src/components/structure/DrawerContent.tsx
+++ web/src/components/structure/DrawerContent.tsx
@@ -2,15 +2,19 @@ import { Home } from "@mui/icons-material";
 import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import { Link } from "react-router-dom";
 import ThemeSwitch from "./ThemeSwitch";
+import { useAuth } from "../../contexts/AuthContext";
 
 interface DrawerContentProps {
     onClick: () => void;
 }
 
 function DrawerContent({ onClick }: DrawerContentProps) {
+    const { token, logout } = useAuth();
     return (
         <Box sx={{ width: 250 }} role="presentation" onClick={onClick} onKeyDown={onClick}>
             <List>
+                {token ? (
+                    <>
                 <Link to='/' style={{ textDecoration: 'none', color: 'inherit' }}>
                     <ListItem disablePadding>
                         <ListItemButton>
@@ -21,16 +25,37 @@ function DrawerContent({ onClick }: DrawerContentProps) {
                         </ListItemButton>
                     </ListItem>
                 </Link>
-                <Link to='/leaderboard' style={{ textDecoration: 'none', color: 'inherit' }}>
+                
+                    <Link to='/leaderboard' style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Home />
+                                </ListItemIcon>
+                                <ListItemText primary="Leaderboard" />
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={logout}>
+                            <ListItemIcon>
+                                <Home />
+                            </ListItemIcon>
+                            <ListItemText primary="Logout" />
+                        </ListItemButton>
+                    </ListItem>
+                </>) : <>
+                <Link to='/login' style={{ textDecoration: 'none', color: 'inherit' }}>
                     <ListItem disablePadding>
                         <ListItemButton>
                             <ListItemIcon>
                                 <Home />
                             </ListItemIcon>
-                            <ListItemText primary="Leaderboard" />
+                            <ListItemText primary="Login" />
                         </ListItemButton>
                     </ListItem>
                 </Link>
+                </>}
                 <ThemeSwitch />
             </List>
         </Box>);
--- web/src/components/structure/NavLinks.tsx
+++ web/src/components/structure/NavLinks.tsx
@@ -1,20 +1,39 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import ThemeSwitch from "./ThemeSwitch";
+import { useAuth } from "../../contexts/AuthContext";
 
 function NavLinks() {
+    const { token, logout } = useAuth();
     return (
         <Box sx={{ display: 'flex', gap: 4, alignItems: 'center' }}>
-            <Link to='/leaderboard' style={{ color: 'inherit' }}>
-                <Typography variant='h6'>
-                    Leaderboard
-                </Typography>
-            </Link>
-            <Link to='/upload' style={{ color: 'inherit' }}>
-                <Typography variant='h6'>
-                    Upload
-                </Typography>
-            </Link>
+            {token ? (
+                <>
+                <Link to='/leaderboard' style={{ color: 'inherit' }}>
+                    <Typography variant='h6'>
+                        Leaderboard
+                    </Typography>
+                </Link>
+                <Link to='/upload' style={{ color: 'inherit' }}>
+                    <Typography variant='h6'>
+                        Upload
+                    </Typography>
+                </Link>
+                <Button color="inherit" onClick={logout}>
+                    <Typography variant='h6'>
+                        Logout
+                    </Typography>
+                </Button>
+                </>
+            ) : (
+                <>
+                <Link to='/login' style={{ color: 'inherit' }}>
+                    <Typography variant='h6'>
+                        Login
+                    </Typography>
+                </Link>
+                </>
+            )}
             <ThemeSwitch />
         </Box>
     );
