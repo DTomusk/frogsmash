e79cf47712ca06e78ee4f892a8186cacd2e324dd
Dave <david.tomusk@gmail.com>
2025-12-02 21:23:02 +0000

    Revert vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/frogsmash/',
+  base: '/',
   server: {
     host: '0.0.0.0',  // ensures Vite is reachable from Docker
     port: 5173,
