f73349d3ee76def7ec47d1f3aa26648c2e5beb40
Dave <david.tomusk@gmail.com>
2025-12-02 20:37:46 +0000

    Add base to vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/frogsmash/',
   server: {
     host: '0.0.0.0',  // ensures Vite is reachable from Docker
     port: 5173,
