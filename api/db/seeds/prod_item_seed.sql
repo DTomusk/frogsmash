d25674852dead854ed4c33e2bbe40f755db6d6fa
Dave <david.tomusk@gmail.com>
2025-12-06 20:35:26 +0000

    Add new charcters to seed script

--- api/db/seeds/prod_item_seed.sql
+++ api/db/seeds/prod_item_seed.sql
@@ -1,3 +1,6 @@
 INSERT INTO items (name, image_url, score, license) VALUES 
 ('Lil Cheese', 'https://live.staticflickr.com/3186/2725343507_c40d7f8a59_b.jpg', 1000.0000, 'CC BY 2.0'),
-('Geoffrey', 'https://live.staticflickr.com/82/249724284_1af270f072_b.jpg', 1000.0000, 'CC BY-NC-ND 2.0')
+('Geoffrey', 'https://live.staticflickr.com/82/249724284_1af270f072_b.jpg', 1000.0000, 'CC BY-NC-ND 2.0'),
+('Bartholomew', 'https://live.staticflickr.com/5265/5622217744_4acf2ed837_b.jpg', 1000.0000, 'CC BY 2.0'),
+('Spickle Pickle', 'https://pub-7a23e348b3054580a5ec15b0c45cc8d7.r2.dev/0b9f8161-0cda-48a6-b4b8-1fd5b4a09c77.jpeg', 1000.0000, 'Submitted by user'),
+('FrogSmash CEO', 'https://pub-7a23e348b3054580a5ec15b0c45cc8d7.r2.dev/PXL_20230924_115555029.jpg', 1000.0000, 'Submitted by user');
