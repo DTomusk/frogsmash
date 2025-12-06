c0c5aab3b61ef5114db5b0ad459cc1a9ffb798b8
Dave <david.tomusk@gmail.com>
2025-12-06 00:49:54 +0000

    Add prod seed

---
+++ api/db/seeds/prod_item_seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO items (name, image_url, score, license) VALUES 
+('Lil Cheese', 'https://live.staticflickr.com/3186/2725343507_c40d7f8a59_b.jpg', 1000.0000, 'CC BY 2.0'),
+('Geoffrey', 'https://live.staticflickr.com/82/249724284_1af270f072_b.jpg', 1000.0000, 'CC BY-NC-ND 2.0')
